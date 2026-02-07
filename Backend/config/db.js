243be6d85b760c2e910f9c83b82febbb46c05c64
Ashutosh Pratap Mall <ashupratap.26@gmail.com>
2026-02-07 10:18:15 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
+
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("db connected successfully!");
-    } catch (err) {
-         console.error("DB connection failed", err);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("DB connected successfully!");
+  } catch (err) {
+    console.error("DB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
 export default connectDb;
