cab6a89fbb91ffaa0992a78c0c5f7f1c49e1faa0
Ashutosh Pratap Mall <ashupratap.26@gmail.com>
2026-02-07 10:33:15 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 let isConnected = false;
 const connectDb = async () => {
+  if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
