da8b4283888f86f5f58a922b65a808909c759c6c
Ashutosh Pratap Mall <ashupratap.26@gmail.com>
2026-02-07 11:19:41 +0530

    Update isAuth.js

--- Backend/middlewares/isAuth.js
+++ Backend/middlewares/isAuth.js
@@ -1,16 +1,23 @@
-import "dotenv/config";
 import jwt from "jsonwebtoken";
-const isAuth = async (req, res, next) => {
+
+const isAuth = (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies?.token;
+
     if (!token) {
-      return res.status(404).json({message: "Not authorized, no token"});
+      return res.status(401).json({
+        message: "Not authorized, token missing",
+      });
     }
-    const decode = jwt.verify(token, process.env.JWT_SECRET).userId;
-    req.userId = decode;
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.userId;
     next();
   } catch (err) {
-    return res.status(401).json({message: "Token is invalid or expired"});
+    return res.status(401).json({
+      message: "Token invalid or expired",
+    });
   }
 };
 
